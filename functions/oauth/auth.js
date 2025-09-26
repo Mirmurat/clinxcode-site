44870c70edaf395598804c22be39d0b14b3cd1a3
Mirmurat <akaymurat.a@gmail.com>
2025-09-26 20:01:30 +0200

    Update and rename functions:oauth:auth.js to functions/oauth/auth.js

---
+++ functions/oauth/auth.js
@@ -0,0 +1,20 @@
+export async function onRequest({ env, request }) {
+  const origins = (env.ALLOWED_ORIGINS || 'https://clinxcode.com').split(',').map(s=>s.trim());
+  const origin  = request.headers.get('Origin') || origins[0];
+  const allow   = origins.includes(origin) ? origin : origins[0];
+
+  if (request.method === 'OPTIONS') {
+    return new Response('', {
+      headers: {
+        'Access-Control-Allow-Origin':  allow,
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Methods': 'GET,POST,OPTIONS'
+      }
+    });
+  }
+
+  const u = new URL('https://github.com/login/oauth/authorize');
+  u.searchParams.set('client_id', env.Ov23liie6U56gCNxlgjZ);
+  u.searchParams.set('scope', 'repo');
+  return Response.redirect(u.toString(), 302);
+}
