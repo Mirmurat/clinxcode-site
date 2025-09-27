a097d02624a635941d675d9e5ee70e391aeeb8c9
Mirmurat <akaymurat.a@gmail.com>
2025-09-27 13:10:30 +0200

    Update init.js

--- admin/init.js
+++ admin/init.js
@@ -1,14 +1,1 @@
-// v2: güvenli init + küçük debug
-(function () {
-  try {
-    console.log('[admin] init.js loaded');
-    if (typeof CMS === 'undefined') throw new Error('CMS global not found');
-    CMS.init();
-    console.log('[admin] CMS.init() called');
-  } catch (e) {
-    console.error('[admin] init error', e);
-    document.body.insertAdjacentHTML('beforeend',
-      '<pre style="white-space:pre-wrap;background:#fee2e2;border:1px solid #fecaca;padding:12px;border-radius:8px">' +
-      (e.stack || e) + '</pre>');
-  }
-})();
+
